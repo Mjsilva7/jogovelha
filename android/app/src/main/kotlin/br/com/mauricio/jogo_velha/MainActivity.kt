15dd02f4206640de5da65ce09fa97a5ebf2a8413
Mjsilva7 <mj.silva7@gmail.com>
2021-05-11 20:34:34 -0300

    primeiro

---
+++ android/app/src/main/kotlin/br/com/mauricio/jogo_velha/MainActivity.kt
@@ -0,0 +1,6 @@
+package br.com.mauricio.jogo_velha
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/core/constants.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+const int BOARD_SIZE = 9;
+
+const Color PLAYER1_COLOR = Colors.purple;
+const Color PLAYER2_COLOR = Colors.black;
+
+const String PLAYER1_SYMBOL = 'X';
+const String PLAYER2_SYMBOL = '0';
+
+const String GAME_TITLE = 'Jogo da Velha';
+const String TIED_TITLE = 'Game tied!';
+const String WIN_TITLE = 'Player [SYMBOL] won!';
+const String DIALOG_MESSAGE = 'Pressione o botão para iniciar!';
+const String SINGLE_PLAYER_MODE_LABEL = 'Single Player';
+const String MULTIPLAYER_MODE_LABEL = 'Dois jogadores';
+const String RESET_BUTTON_LABEL = 'Reset';
